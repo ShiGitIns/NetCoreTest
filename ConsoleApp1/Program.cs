57402dd2630219f709598b21501532ae5c2022dc
shichunjiegit <45807571+shichunjiegit@users.noreply.github.com>
2018-12-12 16:00:22 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string str= Console.ReadLine();
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
